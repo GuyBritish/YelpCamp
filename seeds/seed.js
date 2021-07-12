697a598cb3cdd826ad65897363187b9b5c983431
Max <80323354+GuyBritish@users.noreply.github.com>
2021-07-12 21:40:31 +0700

    update seed generator

--- seeds/seed.js
+++ seeds/seed.js
@@ -19,14 +19,16 @@ const seed = require("./seedList");
 
 async function seedDB() {
 	await Campground.deleteMany({});
-	let randNum, camp;
+	let randLocation, randName1, randName2, camp;
 	for (let i = 0; i <= 49; ++i) {
-		randNum = Math.floor(Math.random() * 1000);
+		randLocation = Math.floor(Math.random() * seed.cities.length);
+		randName1 = Math.floor(Math.random() * seed.descriptors.length);
+		randName2 = Math.floor(Math.random() * seed.places.length);
 		camp = new Campground({
-			title: `${seed.descriptors[randNum % 18]} ${seed.places[randNum % 21]}`,
+			title: `${seed.descriptors[randName1]} ${seed.places[randName2]}`,
 			price: "$15",
 			description: `Camp #${i + 1}`,
-			location: `${seed.cities[randNum].city}, ${seed.cities[randNum].state}`,
+			location: `${seed.cities[randLocation].city}, ${seed.cities[randLocation].state}`,
 		});
 		await camp.save();
 	}
