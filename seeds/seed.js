ff8bd1b728578419e1e93ce29ceb13686e58b5dc
Max <80323354+GuyBritish@users.noreply.github.com>
2021-07-12 19:15:15 +0700

    add seeds for database

---
+++ seeds/seed.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/Campground");
+
+mongoose.connect("mongodb://localhost:27017/YelpCamp", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverSelectionTimeoutMS: 10000,
+});
+
+const dbConnect = mongoose.connection;
+dbConnect.on("error", console.error.bind(console, "Database connection error:"));
+dbConnect.once("open", () => {
+	console.log("Connected to database");
+});
+
+//=================================================================================================
+
+const seed = require("./seedList");
+
+async function seedDB() {
+	await Campground.deleteMany({});
+	let randNum, camp;
+	for (let i = 0; i <= 49; ++i) {
+		randNum = Math.floor(Math.random() * 1000);
+		camp = new Campground({
+			title: `Camp ${i + 1}`,
+			price: "$15",
+			description: `${seed.descriptors[randNum % 18]} ${seed.places[randNum % 21]}`,
+			location: `${seed.cities[randNum].city}, ${seed.cities[randNum].state}`,
+		});
+		await camp.save();
+	}
+}
+
+//=================================================================================================
+
+seedDB();
