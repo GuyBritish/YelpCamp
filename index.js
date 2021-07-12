7a51a657739e2e4d0bfdfcac94b1f8e4cb8fc21d
Max <80323354+GuyBritish@users.noreply.github.com>
2021-07-12 17:29:30 +0700

    Setup database

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ app.use(express.urlencoded({ extended: true }));
 
 //=================================================================================================
 
+const mongoose = require("mongoose");
+const Campground = require("./models/Campground");
+
+mongoose.connect("mongodb://localhost:27017/YelpCamp", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverSelectionTimeoutMS: 10000,
+});
+
+const dbConnect = mongoose.connection;
+dbConnect.on("error", console.error.bind(console, "Database connection error:"));
+dbConnect.once("open", () => {
+	console.log("Connected to database");
+});
+
+//=================================================================================================
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
