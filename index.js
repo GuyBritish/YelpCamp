6f5765a6ced58c533ea89c6a030486ecee5f9741
Max <80323354+GuyBritish@users.noreply.github.com>
2021-07-19 00:37:47 +0700

    add static asset directory

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride("_method"));
