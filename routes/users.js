cdb17e0f6f723ef4c28203c279475bd7ba456df2
Max <80323354+GuyBritish@users.noreply.github.com>
2021-07-20 18:46:56 +0700

    add login routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const passport = require("passport");
 
 const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
@@ -11,6 +12,19 @@ const User = require("../models/User");
 router.get("/register", (req, res) => {
 	res.render("users/register");
 });
+
+router.get("/login", (req, res) => {
+	res.render("users/login");
+});
+
+router.post(
+	"/login",
+	passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }),
+	(req, res) => {
+		req.flash("success", "Welcome back!");
+		res.redirect("/campgrounds");
+	}
+);
 
 router.post(
 	"/register",
