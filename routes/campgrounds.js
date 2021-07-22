0d574a7824a50450335fd32903033fa4698469bf
Max <80323354+GuyBritish@users.noreply.github.com>
2021-07-22 07:06:57 +0700

    Refractor controllers

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,101 +3,25 @@ const router = express.Router({ mergeParams: true });
 
 const catchAsync = require("../utils/catchAsync");
 
-const Campground = require("../models/Campground");
-
 const { isAuth, checkAuthorCamp, validateCamp } = require("../middleware");
+
+const Control = require("../controllers/campgrounds_Control");
 
 //=================================================================================================
 
-router.get(
-	"/",
-	catchAsync(async (req, res) => {
-		const camps = await Campground.find({});
-		res.render("campgrounds/index", { camps });
-	})
-);
+router.get("/", catchAsync(Control.indexPage));
 
-router.get("/new", isAuth, (req, res) => {
-	res.render("campgrounds/new");
-});
+router.get("/new", isAuth, Control.newCampForm);
 
-router.get(
-	"/:id",
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
+router.get("/:id", catchAsync(Control.showCamp));
 
-		const camp = await Campground.findById(id)
-			.populate({
-				path: "reviews",
-				populate: {
-					path: "author",
-				},
-			})
-			.populate("author");
+router.get("/:id/edit", isAuth, checkAuthorCamp, catchAsync(Control.editCampForm));
 
-		if (!camp) {
-			req.flash("error", "Couldn't find that campground!");
-			return res.redirect("/campgrounds");
-		}
-		res.render("campgrounds/show", { camp });
-	})
-);
+router.post("/", isAuth, validateCamp, catchAsync(Control.createCamp));
 
-router.get(
-	"/:id/edit",
-	isAuth,
-	checkAuthorCamp,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const camp = await Campground.findById(id);
-		if (!camp) {
-			req.flash("error", "Couldn't find that campground!");
-			return res.redirect("/campgrounds");
-		}
-		res.render("campgrounds/edit", { camp });
-	})
-);
+router.put("/:id", isAuth, checkAuthorCamp, validateCamp, catchAsync(Control.editCamp));
 
-router.post(
-	"/",
-	isAuth,
-	validateCamp,
-	catchAsync(async (req, res, next) => {
-		//if (!req.body.newCamp) throw new ExpressError(400, "Invalid Campground Data");
-		const { title, price, location } = req.body.newCamp;
-		const camp = new Campground(req.body.newCamp);
-		camp.author = req.user._id;
-		await camp.save();
-		req.flash("success", "Successfully created a new campground!");
-		res.redirect(`/campgrounds/${camp._id}`);
-	})
-);
-
-router.put(
-	"/:id",
-	isAuth,
-	checkAuthorCamp,
-	validateCamp,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const { name, price, location } = req.body.newCamp;
-		const targetCamp = await Campground.findByIdAndUpdate(id, req.body.newCamp);
-		req.flash("success", "Successfully updated campground!");
-		res.redirect(`/campgrounds/${targetCamp._id}`);
-	})
-);
-
-router.delete(
-	"/:id",
-	isAuth,
-	checkAuthorCamp,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const deletedCamp = await Campground.findByIdAndDelete(id);
-		req.flash("success", "Successfully deleted campground!");
-		res.redirect("/campgrounds");
-	})
-);
+router.delete("/:id", isAuth, checkAuthorCamp, catchAsync(Control.deleteCamp));
 
 //=================================================================================================
 
