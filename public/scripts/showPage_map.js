854f1e46cc118029684f32c30499c4c96a566b6d
Max <80323354+GuyBritish@users.noreply.github.com>
2021-07-25 04:04:31 +0700

    add marker popup

--- public/scripts/showPage_map.js
+++ public/scripts/showPage_map.js
@@ -1,9 +1,16 @@
 mapboxgl.accessToken = mbxToken;
-var map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
 	container: "map", // container ID
-	style: "mapbox://styles/mapbox/streets-v11", // style URL
+	style: "mapbox://styles/mapbox/light-v10", // style URL
 	center: camp.geometry.coordinates, // starting position [lng, lat]
 	zoom: 9, // starting zoom
 });
 
-var marker = new mapboxgl.Marker({}).setLngLat(camp.geometry.coordinates).addTo(map);
+let marker = new mapboxgl.Marker({})
+	.setLngLat(camp.geometry.coordinates)
+	.setPopup(
+		new mapboxgl.Popup({ offset: 25 }).setHTML(
+			`<h4> ${camp.title} </h4> <p> ${camp.location} </p>`
+		)
+	)
+	.addTo(map);
